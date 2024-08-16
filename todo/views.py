59192ce14d7b56e4e1313a7e8fe91045db131d57
jiyoung.Moon <whaehofk521@gmail.com>
2024-08-16 15:25:03 +0900

    Initial commit

---
+++ todo/views.py
@@ -0,0 +1,63 @@
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import render, redirect
+
+class TodoListView(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class TodoDetailView(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    lookup_field = "id" # URL에서 전달된 pk(기본키)를 사용해 특정 Todo 항목을 조회
+
+class TodoCreateView(generics.CreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class TodoUpdateView(generics.UpdateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    lookup_field = "id"
+
+class TodoCompleteView(APIView):
+    def post(self, request, id):
+        try:
+            todo = Todo.objects.get(id=id)
+            todo.is_completed = True
+            todo.save()
+            serializer = TodoSerializer(todo)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Todo.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+class TodoDeleteView(generics.DestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    lookup_field = "id"
+
+# 하단부터 Django View 함수를 작성합니다.
+def todo_list(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Todo.objects.create(title=title)
+        return redirect('todo-list')
+
+    todos = Todo.objects.all()
+    return render(request, 'todo_list.html', {'todos': todos})
+
+def todo_complete(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    todo.is_completed = True
+    todo.save()
+    return redirect('todo-list')
+
+def todo_delete(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    todo.delete()
+    return redirect('todo-list')
